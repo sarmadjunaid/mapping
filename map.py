6f35e4c3fd0724ab974c44980329845616d7ae10
Sarmad Junaid <sarmadjkhan@gmail.com>
2021-12-23 03:07:02 +0500

    Added markers through reading file

--- map.py
+++ map.py
@@ -1,10 +1,20 @@
 import folium
+import pandas
+from pandas.io import html
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+name = list(data["NAME"])
+type = list(data["TYPE"])
 
 map = folium.Map(location=[31.452636429987884, 74.36261585417512], tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-fg.add_child(folium.Marker(location=[31.446479113224488, 74.40758976889724], popup="Sarmad's Home", icon=folium.Icon(color='blue')))
+for lt, ln, n, t in zip(lat, lon, name, type):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(html=f"<b>Name:</b> {n} <br> <b>Type:</b> {t}", max_width=80), icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 
