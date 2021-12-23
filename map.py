5ca1409089d0a0434de2de6789efee6dc66134cd
Sarmad Junaid <sarmadjkhan@gmail.com>
2021-12-23 15:08:20 +0500

    changed marker colour according to elevation

--- map.py
+++ map.py
@@ -1,3 +1,4 @@
+from branca.element import IFrame
 import folium
 import pandas
 from pandas.io import html
@@ -8,13 +9,36 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 name = list(data["NAME"])
 type = list(data["TYPE"])
+elev = list(data["ELEV"])
+
+html = """
+    <b>Name: </b> %s
+    <br>
+    <b>Type: </b> %s
+    <br>
+    <b>Elevation: </b> %s
+"""
+
+def markerColor(elev):
+    if elev > 4000:
+        return "darkred"
+    elif elev > 3000:
+        return "red"
+    elif elev > 2000:
+        return "orange"
+    elif elev > 1000:
+        return "darkblue"
+    else:
+        return "blue"
+
 
 map = folium.Map(location=[31.452636429987884, 74.36261585417512], tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, n, t in zip(lat, lon, name, type):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(html=f"<b>Name:</b> {n} <br> <b>Type:</b> {t}", max_width=80), icon=folium.Icon(color="red")))
+for lt, ln, n, t, el in zip(lat, lon, name, type, elev):
+    iframe = folium.IFrame(html=html % (n, t, el), width=200, height=60)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=markerColor(el))))
 
 map.add_child(fg)
 
